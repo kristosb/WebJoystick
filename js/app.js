d8f9537a513ee6ff7e8e0d9c263aabc32baf1e31
Krystian <>
2020-12-27 23:16:36 +0100

    added steering joystick handling

--- js/app.js
+++ js/app.js
@@ -8,16 +8,16 @@ var removeClass = function (el, clss) {
     el.className = el.className.replace(new RegExp('\\b' + clss + ' ?\\b', 'g'), '');
 }
 
-var fullScreenState = true;
+
 var bottomControls = {
     t:TD.toggle({x:10,y:10,width:90,height:90,label:"Front",value:1,name:"toggle"}),
-    b:TD.button({x:110,y:10,width:90,height:90,label:"FullScreen",value:0,name:"button",glyph: "&#xe200", onchange:function(e){bottomControls.log.log(fullScreenState); fullScreenState? openFullscreen():closeFullscreen(); fullScreenState=!fullScreenState;}}),
+    b:TD.button({x:110,y:10,width:90,height:90,label:"FullScreen",value:0,name:"button",glyph: "&#xe200", onchange:function(e){bottomControls.log.log("fs"); toggleFullScreen();}}),
     log:TD.log({x:210,y:10,width:190,height:90,label:"Log",text:"A\nB\nC"}),
-    db:TD.button({x:410,y:10,width:90,height:90,label:"del",value:0,name:"delb",glyph: "&#xe200",onchange:function(e){toggleFullScreen();}}),
+    //db:TD.button({x:410,y:10,width:90,height:90,label:"del",value:0,name:"delb",glyph: "&#xe200",onchange:function(e){toggleFullScreen();}}),
     };
 var middleControls = {
     acc:TD.gauge({x:0,y:0,width:200,height:180,label:"RPMx1000",value:0,min:0,max:10,name:"acc"}),
-    speed:TD.gauge({x:0,y:180,width:200,height:180,label:"km/h",value:0,min:0,max:200,name:"speed"}),
+    speed:TD.gauge({x:0,y:180,width:200,height:180,label:"km/h",value:100,min:0,max:200,name:"speed"}),
     fuel:TD.gauge({x:0,y:360,width:110,height:100,label:"Fuel",value:5,min:0,max:9,name:"speed"}),
     gear:TD.label({x:110,y:360,width:90,height:100,label:"gear",text:"N"})
     };
@@ -34,42 +34,29 @@ var elDebugR = sId('debugR');
 var elDump = elDebug.querySelector('.dump');
 var elDumpR = elDebugR.querySelector('.dump');
 
-var els = {
-    position: {
-        x: elDebug.querySelector('.position .x .data'),
-        y: elDebug.querySelector('.position .y .data')
-    },
-    force: elDebug.querySelector('.force .data'),
-    pressure: elDebug.querySelector('.pressure .data'),
-    distance: elDebug.querySelector('.distance .data'),
-    angle: {
-        radian: elDebug.querySelector('.angle .radian .data'),
-        degree: elDebug.querySelector('.angle .degree .data')
-    },
-    direction: {
-        x: elDebug.querySelector('.direction .x .data'),
-        y: elDebug.querySelector('.direction .y .data'),
-        angle: elDebug.querySelector('.direction .angle .data')
-    }
-};
-var elsR = {
-    position: {
-        x: elDebugR.querySelector('.position .x .data'),
-        y: elDebugR.querySelector('.position .y .data')
-    },
-    force: elDebugR.querySelector('.force .data'),
-    pressure: elDebugR.querySelector('.pressure .data'),
-    distance: elDebugR.querySelector('.distance .data'),
-    angle: {
-        radian: elDebugR.querySelector('.angle .radian .data'),
-        degree: elDebugR.querySelector('.angle .degree .data')
-    },
-    direction: {
-        x: elDebugR.querySelector('.direction .x .data'),
-        y: elDebugR.querySelector('.direction .y .data'),
-        angle: elDebugR.querySelector('.direction .angle .data')
-    }
-};
+function debugContainer(debugId){
+    var elements  = {
+        position: {
+            x: debugId.querySelector('.position .x .data'),
+            y: debugId.querySelector('.position .y .data')
+        },
+        force: debugId.querySelector('.force .data'),
+        pressure: debugId.querySelector('.pressure .data'),
+        distance: debugId.querySelector('.distance .data'),
+        angle: {
+            radian: debugId.querySelector('.angle .radian .data'),
+            degree: debugId.querySelector('.angle .degree .data')
+        },
+        direction: {
+            x: debugId.querySelector('.direction .x .data'),
+            y: debugId.querySelector('.direction .y .data'),
+            angle: debugId.querySelector('.direction .angle .data')
+        }
+    };
+    return elements;
+}
+var els = debugContainer(elDebug);
+var elsR = debugContainer(elDebugR);
 
 createNipple();
 
@@ -77,8 +64,10 @@ function bindNipple () {
     joystick.on('start end', function (evt, data) {
         dump(evt.type,elDump);
         debug(data, els);
+        readJoystick(evt.type,data,middleControls);
     }).on('move', function (evt, data) {
         debug(data,els);
+        readJoystick(evt.type,data,middleControls);
     }).on('dir:up plain:up dir:left plain:left dir:down ' +
         'plain:down dir:right plain:right',
         function (evt, data) {
@@ -91,19 +80,19 @@ function bindNipple () {
     joystickR.on('start end', function (evt, data) {
         dump(evt.type,elDumpR);
         debug(data,elsR);
-        readJoystick(evt.type,data,middleControls);
+        readJoystickR(evt.type,data,middleControls);
     }).on('move', function (evt, data) {
         debug(data,elsR);
-        readJoystick(evt.type,data,middleControls);
+        readJoystickR(evt.type,data,middleControls);
     }).on('dir:up plain:up dir:left plain:left dir:down ' +
         'plain:down dir:right plain:right',
         function (evt, data) {
             dump(evt.type, elDumpR);
-            readJoystick(evt.type,data,middleControls);
+            //readJoystick(evt.type,data,middleControls);
         }
     ).on('pressure', function (evt, data) {
         debug({pressure: data},elsR);
-        readJoystick(evt.type,data,middleControls);
+        //readJoystick(evt.type,data,middleControls);
     });
 }
 
@@ -115,7 +104,8 @@ function createNipple () {
         color: 'green',
         size: 200,
         lockX: true,
-        restPosition: {x:0,y:0}
+        restPosition: {x:0,y:0},
+        threshold : 0
     });
     joystickR = nipplejs.create({
         zone: document.getElementById('right'),
@@ -147,38 +137,43 @@ function debug (obj, elsIn) {
         parseObj(obj, elsIn);
     }, 0);
 }
-// Print data into dash
-function readJoystick (event , obj, ctrlIn) {
-    function readObj(evt,sub, el) {
-        var acceleration =0;
-        var angle = 0;
-        for (var i in sub) {
-            if (typeof sub[i] === 'object' && el ) {
-                //var x = sub[i];
-                //readObj(sub[i],el);
-                for (var j in sub[i]) {
-                    if (j==='angle') {
-                        angle = sub[j].degree;
-                    }
-                }
-            } else if (el) {
-                if (i==='distance') {
-                    acceleration = sub[i];                  
+//convert dual direction slide into single value
+function readObj(evt,sub, el, left, right, end) {
+    var acceleration =end;
+    var angle = 0;
+    for (var i in sub) {
+        if (typeof sub[i] === 'object' && el ) {
+            for (var j in sub[i]) {
+                if (j==='angle') {
+                    angle = sub[j].degree;
                 }
             }
+        } else if (el) {
+            if (i==='distance') {
+                acceleration = sub[i];                  
+            }
         }
-        if (angle === 270){
-            acceleration = Math.abs(acceleration-100);
-            el.acc.setValue(acceleration/20);
-        }
-        if (angle === 90){
-            acceleration = acceleration+100;
-            el.acc.setValue(acceleration/20);
-        }
-        if(evt === "end") el.acc.setValue(0);
     }
+    if (angle === left){
+        acceleration = Math.abs(acceleration-100);
+    }
+    if (angle === right){
+        acceleration = acceleration+100;
+    }
+    if(evt === "end") acceleration = end;
+    return acceleration;
+}
+// Print data into dash
+function readJoystickR (event , obj, ctrlIn) {
     setTimeout(function () {
-        readObj(event,obj,ctrlIn)
+        var acc = readObj(event,obj,ctrlIn, 270, 90, 0);
+        ctrlIn.acc.setValue(acc/20);
+    }, 100);
+}
+function readJoystick (event , obj, ctrlIn) {
+    setTimeout(function () {
+        var acc = readObj(event,obj,ctrlIn, 0, 180, 100);
+        ctrlIn.speed.setValue(acc);
     }, 100);
 }
 
@@ -197,11 +192,6 @@ function dump (evt,elDumpIn) {
         nbEvents += 1;
     }, 0);
 }
-function readJoystickEvents (evt,ctrlIn) {
-    setTimeout(function () {
-        if(evt === "end") ctrlIn.speed.setValue(0);
-    }, 200);
-}
 
 //console.log(JSON.stringify(joystickR.get(1)));
 
@@ -211,28 +201,8 @@ for (var i in gearControls) middle_zone.appendChild(gearControls[i]);
 for (var i in bottomControls) bottom_zone.appendChild(bottomControls[i]);
 for (var i in middleControls) middle_zone.appendChild(middleControls[i]);
 
-/* View in fullscreen */
-function openFullscreen() {
-    if (elem.requestFullscreen) {
-      elem.requestFullscreen();
-    } else if (elem.webkitRequestFullscreen) { /* Safari */
-      elem.webkitRequestFullscreen();
-    } else if (elem.msRequestFullscreen) { /* IE11 */
-      elem.msRequestFullscreen();
-    }
-  }
-  
-  /* Close fullscreen */
-  function closeFullscreen() {
-    if (document.exitFullscreen) {
-      document.exitFullscreen();
-    } else if (document.webkitExitFullscreen) { /* Safari */
-      document.webkitExitFullscreen();
-    } else if (document.msExitFullscreen) { /* IE11 */
-      document.msExitFullscreen();
-    }
-  }
-  function toggleFullScreen() {
+
+function toggleFullScreen() {
     var doc = window.document;
     var docEl = doc.documentElement;
   
